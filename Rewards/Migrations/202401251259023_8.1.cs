d200f535d40d395f9b51670a30b2818f1291aef8
andrsil1 <andrsil1@PTLISWNA1152286>
2024-01-25 13:00:04 +0000

    IMAGE_URL update

---
+++ Rewards/Migrations/202401251259023_8.1.cs
@@ -0,0 +1,20 @@
+﻿namespace Rewards.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _81 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Rewards", "IMAGE_URL", c => c.String());
+            DropColumn("dbo.Rewards", "IMAGE");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Rewards", "IMAGE", c => c.Binary());
+            DropColumn("dbo.Rewards", "IMAGE_URL");
+        }
+    }
+}
