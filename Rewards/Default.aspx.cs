16b50fc1ca285c45832943ec2152f640308e1b5e
andrsil1 <andrsil1@PTLISWNA1152286>
2024-01-31 15:35:55 +0000

    .

--- Rewards/Default.aspx.cs
+++ Rewards/Default.aspx.cs
@@ -12,7 +12,7 @@ namespace Rewards
 {
     public partial class _Default : System.Web.UI.Page
     {
-        private int USER_ID = 1;
+        private int USER_ID = 3;
 
         protected void btnClaim_Command(object sender, CommandEventArgs e)
         {
