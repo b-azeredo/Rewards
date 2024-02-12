3997c6b8ed86e359fd37b4912b2acf8a913a7fb0
andrsil1 <andrsil1@PTLISWNA1152286>
2024-02-12 10:53:54 +0000

    RegisterPage v1

--- Rewards/RegisterPage.aspx.cs
+++ Rewards/RegisterPage.aspx.cs
@@ -1,17 +1,143 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.DirectoryServices;
+using Microsoft.Ajax.Utilities;
+using Rewards.Manager;
+using Rewards.DBModel;
+using System.Configuration;
+using System;
 
 namespace Rewards
 {
     public partial class RegisterPage : System.Web.UI.Page
     {
+        private static string EscapeLdapSearchFilter(string searchFilter)
+        {
+            StringBuilder escape = new StringBuilder();
+            for (int i = 0; i < searchFilter.Length; ++i)
+            {
+                char current = searchFilter[i];
+                switch (current)
+                {
+                    case '\\':
+                        escape.Append(@"\5c");
+                        break;
+                    case '*':
+                        escape.Append(@"\2a");
+                        break;
+                    case '(':
+                        escape.Append(@"\28");
+                        break;
+                    case ')':
+                        escape.Append(@"\29");
+                        break;
+                    case '\u0000':
+                        escape.Append(@"\00");
+                        break;
+                    case '/':
+                        escape.Append(@"\2f");
+                        break;
+                    default:
+                        escape.Append(current);
+                        break;
+                }
+            }
+            return escape.ToString();
+        }
+
+        public static bool IsAuthenticated(string usr, string pwd)
+        {
+            bool authenticated = false;
+
+            try
+            {
+                DirectoryEntry entry = new DirectoryEntry(ConfigurationManager.AppSettings["LDAP_HOST"], usr, pwd);
+                DirectorySearcher search = new DirectorySearcher(entry)
+                {
+                    PageSize = int.MaxValue,
+                    Filter = "(&(objectCategory=person)(objectClass=user)(sAMAccountName=" + EscapeLdapSearchFilter(usr) + "))"
+                };
+                search.PropertiesToLoad.Add("c");
+                SearchResult result = search.FindOne();
+
+                if (result == null || (result != null && result.Properties["c"][0] != null && result.Properties["c"][0].ToString() != "PT"))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return authenticated;
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(username.Text) && !String.IsNullOrEmpty(password.Text) && !String.IsNullOrEmpty(email.Text) && !String.IsNullOrEmpty(bossEmail.Text))
+                {
+                    string Username = username.Text;
+                    string Password = password.Text;
+                    if (IsAuthenticated(Username, Password))
+                    {
+                        using (var entities = new Entities2())
+                        {
+                            var user = entities.USER.FirstOrDefault(u => u.NAME == Username && u.ACTIVATED == true);
+                            if (user != null)
+                            {
+                                string script = "messageAlert('There is an already exist account with this user info.');";
+                                ClientScript.RegisterStartupScript(this.GetType(), "ValidationAlert", script, true);
+                            }
+                            else
+                            {
+                                USER NewUser = new USER
+                                {
+                                    NAME = Username,                                    
+                                    EMAIL = email.Text,
+                                    MANAGER_EMAIL = bossEmail.Text,
+                                    ROLE = "EMPLOYEE",
+                                    ACTIVATED = true 
+                                };
+                                entities.USER.Add(NewUser);
+                                entities.SaveChanges();
+                                string script = "messageAlert('Account registred with sucess!.');";
+                                ClientScript.RegisterStartupScript(this.GetType(), "ValidationAlert", script, true);
+                                ClientScript.RegisterStartupScript(this.GetType(), "RedirectToLoginPage", "setTimeout(function() { window.location.href = 'LoginPage.aspx'; }, 2000);", true);
+
+                            }
+                        }
+                    }
+                    else
+                    {
+                        string script = "messageAlert('The user doesn\\'t belong to the company!.');";
+                        ClientScript.RegisterStartupScript(this.GetType(), "ValidationAlert", script, true);
+                    }
+                }
+                else
+                {
+                    string script = "messageAlert('Please fill all the fields.');";
+                    ClientScript.RegisterStartupScript(this.GetType(), "ValidationAlert", script, true);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
     }
 }
--- Rewards/RegisterPage.aspx.designer.cs
+++ Rewards/RegisterPage.aspx.designer.cs
@@ -51,12 +51,12 @@ namespace Rewards
         protected global::System.Web.UI.WebControls.TextBox password;
 
         /// <summary>
-        /// Button1 control.
+        /// btnRegister control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.Button btnRegister;
     }
 }
